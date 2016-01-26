eef94673bd39bee5435123771833c5c05152a74c
Elekezem <hasalready@gmail.com>
2016-01-26 02:55:10 +0000

    change ver name

--- cubegen.py
+++ cubegen.py
@@ -16,28 +16,29 @@ y = list(map(float, cols[2]))
 z = list(map(float, cols[3]))
 siso = list(map(float, cols[4]))
 
-nx = []
-ny = []
-nz = []
-nsiso = []
+mx = []
+my = []
+mz = []
+msiso = []
 
 br = 0.5291772083
 delta = 0.05
 for k in range(len(x)):
-    nx.append(k)
-    ny.append(k)
-    nz.append(k)
-    nsiso.append(k)
-    nx[k] = x[k]*-1
-    ny[k] = y[k]*-1
-    nz[k] = z[k]*1
-    nsiso[k] = siso[k]*1
+    mx.append(k)
+    my.append(k)
+    mz.append(k)
+    msiso.append(k)
+    mx[k] = x[k] * -1
+    my[k] = y[k] * -1
+    mz[k] = z[k] * 1
+    msiso[k] = siso[k] * 1
 with open('cube.cube', 'w', newline='') as csvfile:
     cubewriter = csv.writer(csvfile, delimiter=' ',
                             quotechar=' ', quoting=csv.QUOTE_MINIMAL)
     cubewriter.writerow(['C9H9+ HF/6-311++G(d,p) nmr partial 3d grid'])
     cubewriter.writerow(['x,y,z: -3.5(0.05)3.5 A '
                          'exp. geom. (1.3964/1.0831)'])
+    cubewriter.writerow([18,])
 
     # for c in range(len(x)):
     for c in range(100):
@@ -45,6 +46,6 @@ with open('cube.cube', 'w', newline='') as csvfile:
     pass
     # for nc in range(len(nx)):
     for nc in range(100):
-        cubewriter.writerow([nx[nc], ny[nc], nz[nc], nsiso[nc]])
+        cubewriter.writerow([mx[nc], my[nc], mz[nc], msiso[nc]])
     pass
 csvfile.close()
