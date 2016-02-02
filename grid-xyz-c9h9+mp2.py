a6163ee6cd8bf7aacd854213dd87809f766cb8c0
elekezem <hasalready@gmail.com>
2016-02-02 03:46:19 +0000

    set no reflection

--- grid-xyz-c9h9+mp2.py
+++ grid-xyz-c9h9+mp2.py
@@ -3,9 +3,6 @@
 # (x,y,z) -> (-x,-y,z)
 # and prints GAUSSIAN cube data in x, y, z order
 # (z changes fastest, followed by y and x)
-#
-import os
-import sys
 import csv
 
 # nx = 70
@@ -21,25 +18,40 @@ nx = 70
 ny = 90
 nz = 70
 delta = 0.05
-def GridGen():
+MAXCOLS = 9
+cols = [[] for _ in range(MAXCOLS)]
+
+
+def read_out():
+    with open('c2', 'r') as input:
+        for row in csv.reader(input, delimiter=' '):
+            for i in range(MAXCOLS):
+                cols[i].append(row[i] if i < len(row) else '')
+    return cols[4]
+
+
+def grid_gen():
     nx = 70
     ny = 90
     nz = 70
     dalta = 0.05
-    globals
-    for x in range(-nx,nx+1,1):
+    for x in range(-nx, nx + 1, 1):
         x = x * dalta
-        for y in range(-ny,ny+1,1):
+        for y in range(-ny, ny + 1, 1):
             y = y * dalta
-            for z in range(-nz,nz+1,1):
+            for z in range(-nz + 10, nz + 11, 1):
                 z = z * dalta
                 ix.append("%.6f" % x)
                 iy.append("%.6f" % y)
                 iz.append("%.6f" % z)
     return ix, iy, iz
 
-GridGen()
-print(ix[1])
+
+########################################################################################
+read_out()
+grid_gen()
+print(len(ix))
+print(len(cols[4])*2)
 with open('cube_grid', 'w', newline='') as csvfile:
     cubewriter = csv.writer(csvfile, delimiter='\t',
                             quotechar=' ', quoting=csv.QUOTE_MINIMAL)
